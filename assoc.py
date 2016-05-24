87ec9d7894f2d27a49aa375c214996e6a8383273
wywang <yuanwy.86@gmail.com>
2016-05-24 22:24:00 +0800

    Updated to plot using plotly

--- assoc.py
+++ assoc.py
@@ -1,20 +1,17 @@
-#!/usr/bin/python
-
 import numpy as np
 import csv, sys
 import matplotlib.pyplot as plt
+import plotly
+import plotly.graph_objs as go
+
+plotly.offline.init_notebook_mode() # run at the start of every notebook
+
+import pandas as pd
 
 filename = 'XD edit suvery input template.xlsx - Cleaned Data.csv'
 
 reader = csv.reader(open(filename,'r',encoding="utf8"),delimiter=',')
 data=np.array([line for line in reader])
-
-#print data[2049,151]
-np.set_printoptions(threshold=np.nan)
-
-#data[data=='#N/A'] = 0
-#data[data=='NA'] = 0
-#data[data==''] = 0
 
 try:
     for index, x in np.ndenumerate(data):
@@ -24,7 +21,7 @@ except ValueError:
     print("ERROR!!!")
 
 data = data.astype(float)
-    
+
 correl = np.array([[np.corrcoef(data[:,i],data[:,j])[0,1] for i in range(len(data[0]))] for j in range(len(data[0]))])
 correl = np.around(correl, decimals=2)
 correl[np.isnan(correl)] = 0
@@ -32,6 +29,22 @@ for index, x in np.ndenumerate(correl):
     if index[0]==index[1]:
         correl[index] = 0
 
-heatmap = plt.pcolor(correl,vmin=0,vmax=1)
-plt.colorbar(heatmap)
-plt.show()
+data = [
+    go.Surface(
+        z=correl
+    )
+]
+layout = go.Layout(
+    title='Correlations between Qns',
+    autosize=False,
+    width=1000,
+    height=1000,
+    margin=dict(
+        l=65,
+        r=50,
+        b=65,
+        t=90
+    )
+)
+fig = go.Figure(data=data, layout=layout)
+plotly.offline.plot(fig)
